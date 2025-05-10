360dea6877e12da13789b5ed192aa964f2c7ca5f
sagnikBag <146710246+Sagnikrick568@users.noreply.github.com>
2025-05-10 16:41:07 +0530

    new

--- 01heroproject/src/component/post-form/PostForm.jsx
+++ 01heroproject/src/component/post-form/PostForm.jsx
@@ -2,7 +2,7 @@ import React,{useCallback} from 'react'
 import { useForm } from 'react-hook-form'
 import {Button, Input,Select,RTE} from '../index'
 import appwriteService from '../../appwrite/config'
-import { data, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 function PostForm() {
